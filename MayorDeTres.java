16de31c1dec157f03549e3fd7ffb5583c15e4616
jonvicente10 <jonvicente10@hotmail.com>
2014-09-18 09:20:56 +0200

    Archivo creado

---
+++ MayorDeTres.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+ 
+class MayorDeTres
+{
+   public static void main(String args[])
+   {
+      int x, y, z;
+      System.out.println("Introduce tres numeros ");
+      Scanner in = new Scanner(System.in);
+ 
+      x = in.nextInt();
+      y = in.nextInt();
+      z = in.nextInt();
+ 
+      if ( x > y && x > z )
+         System.out.println("El primer numero es el mayor.");
+      else if ( y > x && y > z )
+         System.out.println("El segundo numero es el mayor.");
+      else if ( z > x && z > y )
+         System.out.println("El tercer numero es el mayor.");
+      else   
+         System.out.println("Todos los numeros introducidos han de ser distintos");
+   }
+}
